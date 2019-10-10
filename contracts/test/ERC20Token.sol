c99d7a506017bd40adfda150d40b4cdd78a974b4
Fabrizio Vigevani <fabriziovigevani@gmail.com>
2019-10-10 19:57:07 -0300

    PR fixes

--- contracts/test/ERC20Token.sol
+++ contracts/test/ERC20Token.sol
@@ -6,7 +6,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract ERC20Token is ERC20, ERC20Detailed {
 
-    constructor(string name, string symbol) ERC20Detailed(name, symbol, 18) public {
-        _mint(msg.sender, 100000e18);
+    constructor(string name, string symbol, uint8 decimals) ERC20Detailed(name, symbol, decimals) public {
+        _mint(msg.sender, 100000 * (10 ** uint256(decimals)));
     }
 }
--- scripts/start.sh
+++ scripts/start.sh
@@ -7,27 +7,43 @@ echo Running app in MODE: \"$MODE\"
 # Exit script as soon as a command fails.
 set -o errexit
 
+# Executes cleanup function at script exit.
+trap cleanup EXIT
+
+cleanup() {
+  # Kill the RPC instance that we started (if we started one and if it's still running).
+  if [ -n "$pid" ] && pgrep -f $pid > /dev/null; then
+    kill -9 $pid
+  fi
+}
+
 startDevchain() {
-  echo Starting devchain
-  npx aragon devchain --verbose > /dev/null &
-  pid=$!
-  sleep 3
-  echo Running devchain with pid ${pid}
+  pid=$(pgrep -f "aragon devchain" | tail -1)
+  if [ -z "$pid" ] 
+  then
+    echo Starting devchain
+    npx aragon devchain --verbose > /dev/null &
+    pid=$!
+    sleep 3
+    echo Running devchain with pid ${pid}
+  else
+    echo Devchain already running [skipped]
+  fi
 }
 
 deployToken() {
   echo Deploying token
-  RESPONSE=$(npx truffle exec ./scripts/deployToken.js "Dai Stablecoin" "DAI")
+  RESPONSE=$(npx truffle exec ./scripts/deployToken.js "Dai Stablecoin" "DAI" 18)
   REQUEST_TOKEN=$(echo "${RESPONSE}" | tail -1)
   echo Request token: ${REQUEST_TOKEN}
 }
 
 runUsingTemplateIPFS() {
-  npx aragon run --template Template --template-init @ARAGON_ENS --template-args ${REQUEST_TOKEN} --files dist
+  npx aragon run --template Template --template-init @ARAGON_ENS --template-args "App token" 0 "APP" ${REQUEST_TOKEN} --files dist
 }
 
 runUsingTemplateHTTP() {
-  npx aragon run --http localhost:8001 --http-served-from ./dist --template Template --template-init @ARAGON_ENS --template-args ${REQUEST_TOKEN}
+  npx aragon run --http localhost:8001 --http-served-from ./dist --template Template --template-init @ARAGON_ENS --template-args "App token" 0 "APP" ${REQUEST_TOKEN} 
 }
 
 startDevchain
