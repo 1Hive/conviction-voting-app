682939b422d2e14fd0c3fd050d1e7c91cff095df
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2019-12-07 18:03:40 +0300

    stake to proposal test added

--- test/conviction-voting.js
+++ test/conviction-voting.js
@@ -5,17 +5,22 @@ const { hash } = require('eth-ens-namehash')
 const deployDAO = require('./helpers/deployDAO')
 
 const ConvictionVoting = artifacts.require('ConvictionVotingApp.sol')
+const MiniMeERC20Mock = artifacts.require('ERC20Token.sol')
 
 const ANY_ADDRESS = '0xffffffffffffffffffffffffffffffffffffffff'
 
 contract('ConvictionVoting', ([appManager, user]) => {
   let app
+  let miniMeMock
 
   beforeEach('deploy dao and app', async () => {
     const { dao, acl } = await deployDAO(appManager)
 
     // Deploy the app's base contract.
     const appBase = await ConvictionVoting.new()
+    miniMeMock = await MiniMeERC20Mock.new('mock', 'MMM', '8', {
+      from: appManager,
+    })
 
     // Instantiate a proxy for the app, using the base contract as its logic implementation.
     const instanceReceipt = await dao.newAppInstance(
@@ -38,7 +43,7 @@ contract('ConvictionVoting', ([appManager, user]) => {
       { from: appManager }
     )
 
-    await app.initialize(0x0, 0x0, 0x0, 9, 2, 2)
+    await app.initialize(miniMeMock.address, 0x0, 0x0, 9, 2, 2)
   })
 
   it('should create proposals', async () => {
@@ -47,5 +52,39 @@ contract('ConvictionVoting', ([appManager, user]) => {
       from: appManager,
     })
     assert.equal(getEventArgument(receipt, 'ProposalAdded', 'title'), title)
+  })
+
+  it('should stake an amount of tokens on proposal - by appManager', async () => {
+    // When MiniMe mock token was created by appManager, he got all the supply, so
+    // `staleToProposal` call passes functions `require`ments.
+    const stakedTokens = 1000
+    const stakesPerAppManager = 1000
+    // let conviction -> should add assert on newly calculated conviction
+    const receipt = await app.stakeToProposal(1, 1000, { from: appManager })
+    assert.equal(
+      getEventArgument(receipt, 'StakeChanged', 'totalTokensStaked'),
+      stakedTokens
+    )
+    assert.equal(
+      getEventArgument(receipt, 'StakeChanged', 'tokensStaked'),
+      stakesPerAppManager
+    )
+  })
+
+  it('should stake an amount of tokens on proposal - by user', async () => {
+    // user should have some tokens
+    await miniMeMock.transfer(user, 1000, { from: appManager })
+    const stakedTokens = 1000
+    const stakesPerAppManager = 1000
+
+    const receipt = await app.stakeToProposal(1, 1000, { from: user })
+    assert.equal(
+      getEventArgument(receipt, 'StakeChanged', 'totalTokensStaked'),
+      stakedTokens
+    )
+    assert.equal(
+      getEventArgument(receipt, 'StakeChanged', 'tokensStaked'),
+      stakesPerAppManager
+    )
   })
 })
