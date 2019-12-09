8311d93f75498177c35a2d8a2be618c737fb21e1
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2019-12-09 14:29:36 +0300

    complete passing tests

--- contracts/test/ERC20Token.sol
+++ contracts/test/ERC20Token.sol
@@ -6,7 +6,10 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract ERC20Token is ERC20, ERC20Detailed {
 
-    constructor(string name, string symbol, uint8 decimals) ERC20Detailed(name, symbol, decimals) public {
-        _mint(msg.sender, 100000 * (10 ** uint256(decimals)));
+    constructor(string name, string symbol, uint8 decimals, uint256 initialSupply)
+        ERC20Detailed(name, symbol, decimals)
+        public
+    {
+        _mint(msg.sender, initialSupply * (10 ** uint256(decimals)));
     }
 }
