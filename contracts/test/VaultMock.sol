344e2bf51529735ae51e1bb817c7c838d50e6e8b
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2019-12-09 22:43:31 +0300

    using open-zeppelin IERC20 interface

--- contracts/test/VaultMock.sol
+++ contracts/test/VaultMock.sol
@@ -1,16 +1,7 @@
 pragma solidity ^0.4.24;
 
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
-}
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 
 
 contract VaultMock {
