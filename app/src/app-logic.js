88091333ced6be1c6e8c6000cfcd8c6deb0fa2cd
Viviane <vivianedias@protonmail.com>
2020-02-25 17:24:20 -0300

    feat(logic): Separate app login in another file
    App logic passes function hooks to whoever calls it, such as
    `selectProposal` or `isSyncing`.

---
+++ app/src/app-logic.js
@@ -0,0 +1,79 @@
+import { useCallback, useMemo } from 'react'
+import { useApi, useAppState, usePath } from '@aragon/api-react'
+// import usePanelState from './hooks/usePanelState'
+import { noop } from './utils'
+
+const PROPOSAL_ID_PATH_RE = /^\/proposal\/([0-9]+)\/?$/
+const NO_PROPOSAL_ID = '-1'
+
+function idFromPath(path) {
+  if (!path) {
+    return NO_PROPOSAL_ID
+  }
+  const matches = path.match(PROPOSAL_ID_PATH_RE)
+  return matches ? matches[1] : NO_PROPOSAL_ID
+}
+
+// Get the proposal currently selected, or null otherwise.
+export function useSelectedProposal(proposals) {
+  const [path, requestPath] = usePath()
+  const { isSyncing } = useAppState()
+
+  // The memoized proposal currently selected.
+  const selectedProposal = useMemo(() => {
+    const id = idFromPath(path)
+
+    // The `isSyncing` check prevents a proposal to be
+    // selected until the app state is fully ready.
+    if (isSyncing || id === NO_PROPOSAL_ID) {
+      return null
+    }
+
+    return proposals.find(proposal => proposal.id === id) || null
+  }, [path, isSyncing, proposals])
+
+  const selectProposal = useCallback(
+    id => {
+      requestPath(String(id) === NO_PROPOSAL_ID ? '' : `/proposal/${id}/`)
+    },
+    [requestPath]
+  )
+
+  return [selectedProposal, selectProposal]
+}
+
+// Create a new proposal
+export function useCreateProposalAction(onDone = noop) {
+  const api = useApi()
+  return useCallback(
+    title => {
+      if (api) {
+        // Don't care about response
+        api['newProposal(string)'](title).toPromise()
+        onDone()
+      }
+    },
+    [api, onDone]
+  )
+}
+
+// Handles the main logic of the app.
+export default function useAppLogic() {
+  const { isSyncing, proposals = [] } = useAppState()
+
+  const [selectedProposal, selectProposal] = useSelectedProposal(proposals)
+  // const newProposalPanel = usePanelState()
+
+  // const actions = {
+  //   createProposal: useCreateProposalAction(newProposalPanel.requestClose),
+  // }
+
+  return {
+    // actions,
+    isSyncing,
+    // newProposalPanel,
+    selectProposal,
+    selectedProposal,
+    proposals,
+  }
+}
