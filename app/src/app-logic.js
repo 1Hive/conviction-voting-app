45ecbff0aeb68bea47f8a697c7bc852e33d30eda
Viviane <vivianedias@protonmail.com>
2020-02-26 23:15:27 -0300

    feat(logic): All app logic is separated in another file, app-logic
    This logic is passed to App and it's child components via props, if necessary.

--- app/src/app-logic.js
+++ app/src/app-logic.js
@@ -1,69 +1,53 @@
-import { useCallback, useMemo } from 'react'
-import { useApi, useAppState, usePath } from '@aragon/api-react'
+import { useState } from 'react'
+import { useAragonApi, useAppState } from '@aragon/api-react'
 // import usePanelState from './hooks/usePanelState'
-import { noop } from './utils'
+// import { noop } from './utils'
+import { toDecimals } from './lib/math-utils'
+import { toHex } from 'web3-utils'
 
-const PROPOSAL_ID_PATH_RE = /^\/proposal\/([0-9]+)\/?$/
-const NO_PROPOSAL_ID = '-1'
-
-function idFromPath(path) {
-  if (!path) {
-    return NO_PROPOSAL_ID
-  }
-  const matches = path.match(PROPOSAL_ID_PATH_RE)
-  return matches ? matches[1] : NO_PROPOSAL_ID
-}
-
-// Get the proposal currently selected, or null otherwise.
-export function useSelectedProposal(proposals) {
-  const [path, requestPath] = usePath()
-  const { isSyncing } = useAppState()
-
-  // The memoized proposal currently selected.
-  const selectedProposal = useMemo(() => {
-    const id = idFromPath(path)
-
-    // The `isSyncing` check prevents a proposal to be
-    // selected until the app state is fully ready.
-    if (isSyncing || id === NO_PROPOSAL_ID) {
-      return null
-    }
-
-    return (
-      proposals.find(proposal => Number(proposal.id) === Number(id)) || null
-    )
-  }, [path, isSyncing, proposals])
-
-  const selectProposal = useCallback(
-    id => {
-      requestPath(String(id) === NO_PROPOSAL_ID ? '' : `/proposal/${id}/`)
-    },
-    [requestPath]
-  )
-
-  return [selectedProposal, selectProposal]
-}
-
-// Create a new proposal
-export function useCreateProposalAction(onDone = noop) {
-  const api = useApi()
-  return useCallback(
-    title => {
-      if (api) {
-        // Don't care about response
-        api['newProposal(string)'](title).toPromise()
-        onDone()
-      }
-    },
-    [api, onDone]
-  )
-}
+// // Create a new proposal
+// export function useCreateProposalAction(onDone = noop) {
+//   const api = useApi()
+//   return useCallback(
+//     title => {
+//       if (api) {
+//         // Don't care about response
+//         api['newProposal(string)'](title).toPromise()
+//         onDone()
+//       }
+//     },
+//     [api, onDone]
+//   )
+// }
 
 // Handles the main logic of the app.
 export default function useAppLogic() {
-  const { isSyncing, proposals = [] } = useAppState()
+  const { api, connectedAccount } = useAragonApi()
+  const { requestToken, convictionStakes } = useAppState()
 
-  const [selectedProposal, selectProposal] = useSelectedProposal(proposals)
+  const [proposalPanel, setProposalPanel] = useState(false)
+
+  const onProposalSubmit = ({
+    title,
+    link,
+    amount,
+    beneficiary,
+    description,
+  }) => {
+    const decimals = parseInt(requestToken.decimals)
+    const decimalAmount = toDecimals(amount.trim(), decimals).toString()
+    api
+      .addProposal(title, toHex(link), decimalAmount, beneficiary, description)
+      .toPromise()
+    setProposalPanel(false)
+  }
+
+  const myStakes =
+    (convictionStakes &&
+      convictionStakes.filter(({ entity }) => entity === connectedAccount)) ||
+    []
+
+  const myLastStake = [...myStakes].pop() || []
   // const newProposalPanel = usePanelState()
 
   // const actions = {
@@ -72,10 +56,10 @@ export default function useAppLogic() {
 
   return {
     // actions,
-    isSyncing,
     // newProposalPanel,
-    selectProposal,
-    selectedProposal,
-    proposals,
+    onProposalSubmit,
+    proposalPanel,
+    setProposalPanel,
+    myLastStake,
   }
 }
