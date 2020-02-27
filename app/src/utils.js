21c89ec19d68308b44a583a9b5cadf90492baab8
Viviane <vivianedias@protonmail.com>
2020-02-26 23:18:41 -0300

    feat(filter): Add filter hooks

--- app/src/utils.js
+++ app/src/utils.js
@@ -1,1 +1,13 @@
 export function noop() {}
+
+export const PROPOSAL_STATUS_OPEN = 1
+export const PROPOSAL_STATUS_ACCEPTED = 2
+
+export function getProposalStatus(proposal) {
+  switch (proposal.state) {
+    case PROPOSAL_STATUS_ACCEPTED:
+      return PROPOSAL_STATUS_ACCEPTED
+    default:
+      return PROPOSAL_STATUS_OPEN
+  }
+}
