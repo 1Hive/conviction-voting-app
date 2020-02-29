435f5f62a789e595592cc6ae073ef9dc92a5e8cf
David Llop <931684+sembrestels@users.noreply.github.com>
2020-03-01 00:31:43 +0100

    Upgrade CLI to v7

--- contracts/Template.sol
+++ contracts/Template.sol
@@ -27,7 +27,7 @@ contract TemplateBase is APMNamehash {
     ENS public ens;
     DAOFactory public fac;
 
-    event DeployInstance(address dao);
+    event DeployDao(address dao);
     event InstalledApp(address appProxy, bytes32 appId);
 
     constructor(DAOFactory _fac, ENS _ens) public {
@@ -116,6 +116,6 @@ contract Template is TemplateBase {
         acl.revokePermission(this, tokenManager, tokenManager.MINT_ROLE());
         acl.setPermissionManager(root, tokenManager, tokenManager.MINT_ROLE());
 
-        emit DeployInstance(dao);
+        emit DeployDao(dao);
     }
 }
