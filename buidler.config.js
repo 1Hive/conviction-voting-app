74c50ded9144b25975952c1754f41fc79c0c5ff3
William Griffiths <willjgriff@gmail.com>
2020-09-03 18:51:57 +0100

    Fix buidler evm test script.

--- buidler.config.js
+++ buidler.config.js
@@ -1,18 +1,15 @@
 const {usePlugin} = require('@nomiclabs/buidler/config')
 
 usePlugin('@nomiclabs/buidler-truffle5')
-usePlugin('buidler-gas-reporter')
+// usePlugin('buidler-gas-reporter') // Must have a ganache instance running but execute with buidlerevm, otherwise errors occur
 
 module.exports = {
-    defaultNetwork: 'localhost',
+    defaultNetwork: 'buidlerevm',
     networks: {
         buidlerevm: {
         },
-        localhost: {
-            url: 'http://localhost:8545',
-            accounts: {
-                mnemonic: "explain tackle mirror kit van hammer degree position ginger unfair soup bonus"
-            }
+        ganache: {
+            url: 'http://localhost:8545'
         }
     },
     solc: {
