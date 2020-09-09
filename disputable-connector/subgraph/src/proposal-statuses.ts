20ecd1f8f9b940fd4b81ea54151185cfcdba97a4
Rodrigo Perez <rodrigopi1989@gmail.com>
2020-09-08 22:09:21 -0300

    status

--- disputable-connector/subgraph/src/proposal-statuses.ts
+++ disputable-connector/subgraph/src/proposal-statuses.ts
@@ -1,3 +1,6 @@
 export const STATUS_ACTIVE = 'Active'
 export const STATUS_CANCELLED = 'Cancelled'
+export const STATUS_CHALLENGED = 'Challenged'
+export const STATUS_DISPUTED = 'Disputed'
 export const STATUS_EXECUTED = 'Executed'
+export const STATUS_REJECTED = 'Rejected'
